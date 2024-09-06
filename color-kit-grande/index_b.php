3134133820562b442c0ae196036e7e40d9ba2716
Daniel Eichhorn <dani.eichhorn@squix.ch>
2024-09-06 22:03:17 +0200

    Add test for php

--- color-kit-grande/index_b.php
+++ color-kit-grande/index_b.php
@@ -537,6 +537,9 @@
                 </div>
             </div>
         </footer>
+        <?php
+        echo 'Now: '. time();
+        ?>
 
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
